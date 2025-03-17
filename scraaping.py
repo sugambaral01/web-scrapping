3fdd3346ccca2e86af256810863310ef4bb0dcb0
sugambaral01 <sugambaral96@gmail.com>
2025-03-17 22:34:54 +0545

    The srapping of jobs lists and their details from nepali jobs website

--- scraaping.py
+++ scraaping.py
@@ -1,1 +1,24 @@
-print("hello world")
+import requests
+from bs4 import BeautifulSoup
+link=requests.get("https://merojob.com/")
+print(link) # this will print the that we can get the requests or not
+#response 200 we can get it easily
+soup=BeautifulSoup(link.text,'html.parser')
+#print(soup.prettify())# this will print in good and clean format
+box=soup.find_all('div',class_="col-md-4 border-right border-bottom job-card")
+for div in box:
+     classs=div.find('a',itemprop='title').text
+     print(classs)
+
+
+
+
+
+
+
+
+
+
+
+
+
